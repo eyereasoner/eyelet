92e46302cdbdf8946e43c6684dd163255e6f74cc
josd <josderoo@gmail.com>
2025-08-13 12:48:09 +0200

    using \033 instead of \e for escape with ANSI escape sequences

--- output-trealla/derived-rule.pl
+++ output-trealla/derived-rule.pl
@@ -2,6 +2,6 @@
 
 answer(ascribed(test,true)).
 
-step(((ascribed(test,true):+type(A,'Dog')):+type(B,'Cat')),type('Minka','Cat'),(ascribed(test,true):+type(C,'Dog'))).
+step(((ascribed(test,true):+type(A,'Dog')):+type(B,'Cat')),type('Minka','Cat'),(ascribed(test,true):+type(A,'Dog'))).
 step((ascribed(test,true):+type(A,'Dog')),type('Charly','Dog'),ascribed(test,true)).
 step((true:+ascribed(test,true)),ascribed(test,true),true).
